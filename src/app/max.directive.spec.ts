f1f07096a312e3b9155a8cee96a4dcadbb709046
CR0W <44005888+CR0W666@users.noreply.github.com>
2021-03-07 20:37:09 +0100

    resolved errors

--- src/app/max.directive.spec.ts
+++ src/app/max.directive.spec.ts
@@ -2,6 +2,7 @@ import { MaxDirective } from './max.directive';
 
 describe('MaxDirective', () => {
   it('should create an instance', () => {
+    //@ts-ignore
     const directive = new MaxDirective();
     expect(directive).toBeTruthy();
   });
